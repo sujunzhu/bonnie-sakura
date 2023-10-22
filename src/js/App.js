f4d9b4dd497cbf88be447b5761dc4d662d86fd77
Sujun Zhu <zhusujun199495@gmail.com>
2023-10-22 13:17:43 -0700

    1. Organize files; 2. Add custom fonts; 3. Include first page

---
+++ src/js/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../css/App.css';
+
+/**
+ * The React App.
+ * @return {Node} an object
+ */
+function App() {
+  return (
+    <div className="body">
+      <div>
+        THIS IS A WEBSITE FOR BONNIE SAKURA
+      </div>
+    </div>
+  );
+}
+
+export default App;
