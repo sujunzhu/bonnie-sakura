d48f018ef53ed289956d34b2ac79fa10c988e05a
Sujun Zhu <zhusujun199495@gmail.com>
2023-10-22 18:45:54 -0700

    1. Add custom scrollbar; 2. Add scroll bar prompt

--- src/js/App.js
+++ src/js/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../css/App.css';
 import Intro from './Intro';
 
@@ -7,9 +7,41 @@ import Intro from './Intro';
  * @return {Node} an object
  */
 function App() {
+  const [showLayover, setShowLayover] = useState(true);
+
+  const handleScroll = () => {
+    const shouldHideLayover =
+      document.body.scrollTop > 30 || document.documentElement.scrollTop > 30;
+    if (shouldHideLayover && showLayover) {
+      setShowLayover(false);
+    } else if (!shouldHideLayover && !showLayover) {
+      setShowLayover(true);
+    }
+  };
+
+  useEffect(() =>{
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('touchmove', handleScroll);
+    return () => {
+      window.addEventListener('scroll', handleScroll);
+      window.addEventListener('touchmove', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="body">
-      <Intro />
+    <div
+      className="portfolio-container"
+      onWheel={()=>handleScroll()}
+    >
+      {
+        showLayover &&
+        <div className="intro-layover">
+          <Intro />
+        </div>
+      }
+      <div className='timeline'>
+        Surprise!!!!!!!!! Coming soon!
+      </div>
     </div>
   );
 }
--- src/js/Intro.js
+++ src/js/Intro.js
@@ -1,21 +1,31 @@
 import React from 'react';
-import '../css/App.css';
+import '../css/intro.css';
+
+const localizedText = {
+  intro1: 'Hi, I am Xiaofan',
+  intro2: ' a digital product designer passionate' +
+    ' about creating delightful' +
+    ' and impactful experiences',
+  scroll: 'Scroll Down',
+};
 
 /**
  * The intro page.
  * @return {Node} the intro page
  */
 const Intro = () => {
-  const intro1 = 'Hi, I am Xiaofan';
-  const intro2 =
-      ' a digital product designer passionate' +
-      ' about creating delightful' +
-      ' and impactful experiences';
   return (
-    <div className='intro'>
-      {intro1}
-      <br/>
-      {intro2}
+    <div className='intro-container'>
+      <div className='intro-text-container'>
+        <div className='intro'>
+          {localizedText.intro1}
+          <br/>
+          {localizedText.intro2}
+        </div>
+      </div>
+      <div className='scroll-down-prompt'>
+        {localizedText.scroll}
+      </div>
     </div>
   );
 };
