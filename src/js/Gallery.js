916ebdf05ee63b432c7dcb1bb2f4084c0ac48c91
Sujun Zhu <zhusujun199495@gmail.com>
2023-10-27 00:10:09 -0700

    Add gallery test

--- src/js/Gallery.js
+++ src/js/Gallery.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import '../css/Gallery.css';
+import {Application} from '@splinetool/runtime';
 
 /**
  * The Gallery page.
  * @return {Node} the gallery page
  */
 const Gallery = () => {
+  const galleryRef = useRef(null);
+
+  useEffect(() => {
+    if (!galleryRef || !galleryRef.current) {
+      return;
+    }
+
+    const app = new Application(galleryRef.current);
+    app.load('https://prod.spline.design/nvKISINGevWKdkig/scene.splinecode');
+  });
+
   return (
-    <div className='gallery-container'>
-      This is Gallery Page
-    </div>
+    <canvas className='gallery-container' width={100} height={100} ref={galleryRef} />
   );
 };
 
